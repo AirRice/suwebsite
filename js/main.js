4d3e123934c1e7dc4aadfdf9d81270ee34e7fde4
jakubzajac <jakubczajac@gmail.com>
2018-11-05 16:58:09 +0000

    menu fixes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 const menuToggle = document.querySelector('#menu-toggle'),
-      menuItems = document.querySelector('#menu-items');
+      menuItems = document.querySelector('#menu-items'),
+      header = document.querySelector('header');
 
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active');
     menuItems.classList.toggle('active');
+    header.classList.toggle('active');
+    document.querySelector('body').classList.toggle('active');
 });
